f6d7d65a0e76c92bf74158dc76b596d8be62c0b6
Training Department <Training Department>
2018-12-21 18:10:45 +0700

    + auto-select input name + remember excluded test type

--- index.php
+++ index.php
@@ -1,33 +1,57 @@
 <?php
 require_once('TTaker.php');
 
+function checkRadioIT($checked, $html) {
+	if($checked)
+	{
+		$html = str_replace('__radio_IT_checked', 'checked', $html);
+		$html = str_replace('__radio_EN_checked', '', $html);
+	}
+	else
+	{
+		$html = str_replace('__radio_IT_checked', '', $html);
+		$html = str_replace('__radio_EN_checked', 'checked', $html);
+	}
+	return $html;
+}
+
+$page = file_get_contents('idx_tempt.html');
+$page = str_replace('__target', 'index.php', $page);
+
 $v = new TTakerList();
 $searchCriteria = "";
 if(isset($_GET['tt_name'])) {
 	$keyword = htmlspecialchars(lms_trim(remove_accents($_GET['tt_name'])));
 	$searchCriteria = "tt_name_ai LIKE '%".$keyword."%'";
-	unset($_GET['tt_name']);
+	$page = str_replace('__name2search', $keyword, $page);
 }
+else
+	$page = str_replace('__name2search', '', $page);
 
 if(isset($_GET['excludedTest'])) {
 	if($searchCriteria != "")
 		$searchCriteria = $searchCriteria." AND ";
-	if($_GET['excludedTest'] == "IT")
+	if($_GET['excludedTest'] == "IT") {
 		$searchCriteria = $searchCriteria." tt_testType < 3";
-	else
+		$page = checkRadioIT(true, $page);
+	}
+	else {
 		$searchCriteria = $searchCriteria." 2 < tt_testType";
-	unset($_GET['excludedTest']);
+		$page = checkRadioIT(false, $page);
+	}
 }
+else
+	$page = checkRadioIT(false, $page);	
 
 if($searchCriteria != "")
 {
 	$v->SetCriteria4SelectQry(" WHERE ".$searchCriteria." ORDER BY tt_name");
 	$v->MkSelQry();
 	$v->Sel();
+	$page = str_replace('__searchResult', $v->PrintTable(), $page);
 }
-
-$page = file_get_contents('idx_tempt.html');
-$page = str_replace('__target', 'index.php', $page);
-$page = str_replace('__searchResult', $v->PrintTable(), $page);
+else
+	$page = str_replace('__searchResult', '', $page);
+	
 echo $page;
 ?>
