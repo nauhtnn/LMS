12e9cfddcd5e1573c755ff7ac0ff8e0745265692
nauhtnn <thuan.nguyenngoc@live.com>
2019-08-02 02:06:46 +0700

    + httpd.conf AllowOverride All + default index.php is LMS, add /c or /cert to access certificate database

---
+++ ttakerIndex.php
@@ -0,0 +1,57 @@
+<?php
+require_once('TTaker.php');
+
+function checkRadioIT($checked, $html) {
+	if($checked)
+	{
+		$html = str_replace('__radio_IT_checked', 'checked', $html);
+		$html = str_replace('__radio_EN_checked', '', $html);
+	}
+	else
+	{
+		$html = str_replace('__radio_IT_checked', '', $html);
+		$html = str_replace('__radio_EN_checked', 'checked', $html);
+	}
+	return $html;
+}
+
+$page = file_get_contents('idx_tempt.html');
+$page = str_replace('__target', 'index.php', $page);
+
+$v = new TTakerList();
+$searchCriteria = "";
+if(isset($_GET['tt_name'])) {
+	$keyword = htmlspecialchars(lms_trim(remove_accents($_GET['tt_name'])));
+	$searchCriteria = "tt_name_ai LIKE '%".$keyword."%'";
+	$page = str_replace('__name2search', $keyword, $page);
+}
+else
+	$page = str_replace('__name2search', '', $page);
+
+if(isset($_GET['testType'])) {
+	if($searchCriteria != "")
+		$searchCriteria = $searchCriteria." AND ";
+	if($_GET['testType'] == "IT") {
+		$searchCriteria = $searchCriteria." 2 < tt_testType";
+		$page = checkRadioIT(true, $page);
+	}
+	else {
+		$searchCriteria = $searchCriteria." tt_testType < 3";
+		$page = checkRadioIT(false, $page);
+	}
+}
+else
+	$page = checkRadioIT(true, $page);
+
+if($searchCriteria != "")
+{
+	$v->SetCriteria4SelectQry(" WHERE ".$searchCriteria." ORDER BY tt_name");
+	$v->MkSelQry();
+	$v->Sel();
+	$page = str_replace('__searchResult', $v->PrintTable(), $page);
+}
+else
+	$page = str_replace('__searchResult', '', $page);
+	
+echo $page;
+?>
